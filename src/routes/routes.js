0c748de443e45df01ce0aedc5c84df611696ab36
guidojorgelopinto <guidojorgelopinto@gmail.com>
2021-08-12 18:50:25 -0300

    21-commit

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -19,9 +19,9 @@ router.post('/api/signin', AuthController.signIn);
 router.post('/api/signup', AuthController.signUp);
 
 // Rutas posts: token valido - buscamos post - validacion de usuario
-router.get('/api/posts', auth, PostController.index);
-router.post('/api/posts', auth, PostPolicy.show, PostController.index);
-router.post('/api/posts/:id', auth, PostPolicy.show, PostController.index);
+router.get('/api/posts', PostController.index);
+router.post('/api/posts', PostPolicy.show, PostController.index);
+router.post('/api/posts/:id', PostPolicy.show, PostController.index);
 router.get('/api/posts/:id', auth, PostController.find, PostPolicy.show, PostController.show);
 router.patch('/api/posts/:id', auth, PostController.find, PostPolicy.update, PostController.update);
 router.delete('/api/posts/:id', auth, PostController.find, PostPolicy.delete, PostController.delete);
